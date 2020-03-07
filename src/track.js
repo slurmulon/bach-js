0f94fd0b8e6c41a7eff9567d5e38264a84f71460
Erik Vavro <me@madhax.io>
2020-03-07 17:37:55 -0500

    :art: :sparkles: Added Track.interval and Track.at methods. Fixed/improved Track.all.

--- src/track.js
+++ src/track.js
@@ -1,6 +1,7 @@
 import { Beat } from './elements'
 import { validate } from './validate'
 
+// TODO: Possibly rename to Bach, Track will just be a Gig construct
 export class Track {
 
   constructor (source) {
@@ -37,12 +38,56 @@ export class Track {
   all (kind = 'note') {
     return this.data.map(beats => {
       return beats
-        .filter(beat => !!beat.data)
-        .map(({ items }) => {
-          return items.filter(item => item && item.kind === kind)
+        //.map(beat => {
+        .filter(beat => {
+          if (beat.data) {
+            // TODO: Move this logic to Beat
+            return beat.items.some(item => {
+              return item && item.kind === kind
+            })
+          }
         })
-        .flat()
+        // .filter(beat => !!beat.data)
+        // .map(({ items }) => {
+        //   return items.filter(item => item && item.kind === kind)
+        // })
+        // .flat()
     }).flat()
+  }
+
+  /**
+   * Determines how long to wait between each interval based on the track and user tempo
+   *
+   * @returns {number}
+   */
+  get interval () {
+    const header = this.headers.tempo
+    const tempos = {
+      init: header,
+      user: this.tempo || header
+    }
+
+    const diff = tempos.user / tempos.init
+
+    return this.headers['ms-per-beat'] / diff
+  }
+
+  /**
+   * Determines the measure and beat found at the provided indices
+   *
+   * @param {number} measureIndex
+   * @param {number} beatIndex
+   * @returns {Object}
+   */
+  at (measureIndex, beatIndex) {
+    try {
+      const measure = this.data[measureIndex]
+      const beat = measure[beatIndex]
+
+      return { measure, beat }
+    } catch (e) {
+      return null
+    }
   }
 
 }
