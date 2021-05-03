5014e03ccb0712c1a8f2104342c22d81edeefbd6
Erik Vavro <me@madhax.io>
2021-05-03 16:11:44 -0400

    [v3] :package: :factory: :recycle: :construction: Upgrading and refactoring towards rollup 2.46.0 (generally working).

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,34 @@
-import json from 'rollup-plugin-json'
-import babel from 'rollup-plugin-babel'
+// import json from 'rollup-plugin-json'
+// import babel from 'rollup-plugin-babel'
 
-export default {
-  entry: 'src/index.js',
-  dest: 'dist/bundle.js',
-  format: 'cjs',
+// export default {
+//   entry: 'src/index.js',
+//   dest: 'dist/bundle.js',
+//   format: 'cjs',
+//   plugins: [
+//     json(),
+//     babel({
+//       presets: [ 'es2015-rollup' ],
+//       babelrc: false
+//     })
+//   ]
+// }
+
+
+import { babel } from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+
+const config = {
+  input: 'src/index.js',
+  output: {
+    dir: 'output',
+    // format: 'esm'
+    format: 'cjs'
+  },
   plugins: [
-    json(),
-    babel({
-      presets: [ 'es2015-rollup' ],
-      babelrc: false
-    })
+    commonjs(),
+    babel({ babelHelpers: 'bundled' })
   ]
 }
+
+export default config;
