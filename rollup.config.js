597028da462a5d01e60d22a988d47c149943856f
Erik Vavro <me@madhax.io>
2021-05-03 22:32:37 -0400

    [v3] :package: :factory: :wrench: :checkered_flag: Expanded upon rollup.config to support multiple module types.

--- rollup.config.js
+++ rollup.config.js
@@ -15,20 +15,68 @@
 // }
 
 
-import { babel } from '@rollup/plugin-babel'
+// import { babel } from '@rollup/plugin-babel'
+// import commonjs from '@rollup/plugin-commonjs'
+
+// const config = {
+//   input: 'src/index.js',
+//   output: {
+//     dir: 'output',
+//     // format: 'esm'
+//     format: 'cjs'
+//   },
+//   plugins: [
+//     commonjs(),
+//     babel({ babelHelpers: 'bundled' })
+//   ]
+// }
+
+// export default config;
+
+import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import babel from '@rollup/plugin-babel'
+import json from '@rollup/plugin-json'
+import pkg from './package.json'
 
-const config = {
-  input: 'src/index.js',
-  output: {
-    dir: 'output',
-    // format: 'esm'
-    format: 'cjs'
+  // browser-friendly UMD build
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'bach-js',
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
+      json(),
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: ['node_modules/**'],
+        babelHelpers: 'bundled'
+      })
+    ]
   },
-  plugins: [
-    commonjs(),
-    babel({ babelHelpers: 'bundled' })
-  ]
-}
 
-export default config;
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify 
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.js',
+    external: ['ajv', 'bach-cljs', 'bach-json-schema', 'teoria'],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      json(),
+      babel({
+        exclude: ['node_modules/**']
+      })
+    ]
+  }
+]
