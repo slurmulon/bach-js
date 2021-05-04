a59800de221de72a9531f907e7eaf7a8498bad42
Erik Vavro <me@madhax.io>
2021-05-03 22:44:22 -0400

    [v3] :package: :factory: :sparkles: Cleaned up around Rollup upgrade, mostly by removing old deps and code.

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,10 @@
-// import json from 'rollup-plugin-json'
-// import babel from 'rollup-plugin-babel'
-
-// export default {
-//   entry: 'src/index.js',
-//   dest: 'dist/bundle.js',
-//   format: 'cjs',
-//   plugins: [
-//     json(),
-//     babel({
-//       presets: [ 'es2015-rollup' ],
-//       babelrc: false
-//     })
-//   ]
-// }
-
-
-// import { babel } from '@rollup/plugin-babel'
-// import commonjs from '@rollup/plugin-commonjs'
-
-// const config = {
-//   input: 'src/index.js',
-//   output: {
-//     dir: 'output',
-//     // format: 'esm'
-//     format: 'cjs'
-//   },
-//   plugins: [
-//     commonjs(),
-//     babel({ babelHelpers: 'bundled' })
-//   ]
-// }
-
-// export default config;
-
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
 import json from '@rollup/plugin-json'
 import pkg from './package.json'
 
-  // browser-friendly UMD build
+// browser-friendly UMD build
 export default [
   {
     input: 'src/index.js',
