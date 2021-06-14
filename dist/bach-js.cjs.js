4690a2b2864039d0f3eac1961d1c6095cc0c6760
Erik Vavro <me@madhax.io>
2021-06-14 17:22:23 -0400

    [v3] :package: :wrench: Bundle for v3.0.0-beta.6

--- dist/bach-js.cjs.js
+++ dist/bach-js.cjs.js
@@ -10,13 +10,13 @@ var _Object$getOwnPropertyDescriptors = require("@babel/runtime-corejs3/core-js-
 
 var _Object$defineProperties = require("@babel/runtime-corejs3/core-js-stable/object/define-properties");
 
-var _defineProperty = require("@babel/runtime-corejs3/helpers/defineProperty");
-
 var _toConsumableArray = require("@babel/runtime-corejs3/helpers/toConsumableArray");
 
 var _classCallCheck = require("@babel/runtime-corejs3/helpers/classCallCheck");
 
 var _createClass = require("@babel/runtime-corejs3/helpers/createClass");
+
+var _defineProperty = require("@babel/runtime-corejs3/helpers/defineProperty");
 
 var _slicedToArray = require("@babel/runtime-corejs3/helpers/slicedToArray");
 
@@ -206,29 +206,35 @@ function notesIntersect(left, right) {
   return _filterInstanceProperty(left).call(left, function (note) {
     return _includesInstanceProperty(right).call(right, note);
   });
-} // TODO: Remove
-
+}
 
 var unitsOf = function unitsOf(source) {
-  return {
-    step: source.units.beat.step,
-    pulse: source.units.beat.pulse,
-    bar: source.units.bar.step,
-    ms: 1 / source.units.time.bar
-  };
+  var _source$units = source.units,
+      beat = _source$units.beat,
+      bar = _source$units.bar,
+      time = _source$units.time;
+  return durationsOf({
+    step: 1,
+    pulse: 1 / (beat.step / beat.pulse),
+    bar: bar.step,
+    ms: 1 / time.step,
+    second: 1 / time.step * 1000
+  });
 };
 
 var timesOf = function timesOf(source) {
-  var _source$units$time = source.units.time,
-      step = _source$units$time.step,
-      pulse = _source$units$time.pulse,
-      bar = _source$units$time.bar;
-  return {
+  source.units;
+  return durationsOf(_objectSpread({
     ms: 1,
-    second: 1000,
-    step: step,
-    pulse: pulse,
-    bar: bar,
+    second: 1000
+  }, source.units.time));
+};
+
+var durationsOf = function durationsOf(units) {
+  var step = units.step,
+      pulse = units.pulse,
+      bar = units.bar;
+  return _objectSpread(_objectSpread({}, units), {}, {
     's': step,
     'p': pulse,
     'm': bar,
@@ -240,7 +246,7 @@ var timesOf = function timesOf(source) {
     '64n': bar / 64,
     '4up': bar - bar / 4,
     '8up': bar - bar / 8
-  };
+  });
 };
 
 var Note = /*#__PURE__*/function () {
@@ -460,13 +466,24 @@ var Durations = /*#__PURE__*/function () {
       return duration / (this.times[as] / this.times[is]);
     }
   }, {
-    key: "metronize",
-    value: function metronize(duration) {
+    key: "unitize",
+    value: function unitize(duration) {
       var _ref2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
           _ref2$is = _ref2.is,
-          is = _ref2$is === void 0 ? 'ms' : _ref2$is,
+          is = _ref2$is === void 0 ? 'step' : _ref2$is,
           _ref2$as = _ref2.as,
           as = _ref2$as === void 0 ? 'pulse' : _ref2$as;
+
+      return duration / (this.units[as] / this.units[is]);
+    }
+  }, {
+    key: "metronize",
+    value: function metronize(duration) {
+      var _ref3 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+          _ref3$is = _ref3.is,
+          is = _ref3$is === void 0 ? 'ms' : _ref3$is,
+          _ref3$as = _ref3.as,
+          as = _ref3$as === void 0 ? 'pulse' : _ref3$as;
 
       var index = this.cast(duration, {
         is: is,
@@ -495,12 +512,12 @@ var Durations = /*#__PURE__*/function () {
   }, {
     key: "clamp",
     value: function clamp(duration) {
-      var _ref3 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-          _ref3$is = _ref3.is,
-          is = _ref3$is === void 0 ? 'step' : _ref3$is,
-          _ref3$min = _ref3.min,
-          min = _ref3$min === void 0 ? 0 : _ref3$min,
-          max = _ref3.max;
+      var _ref4 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+          _ref4$is = _ref4.is,
+          is = _ref4$is === void 0 ? 'step' : _ref4$is,
+          _ref4$min = _ref4.min,
+          min = _ref4$min === void 0 ? 0 : _ref4$min,
+          max = _ref4.max;
 
       var step = this.cast(duration, {
         is: is,
@@ -519,12 +536,12 @@ var Durations = /*#__PURE__*/function () {
   }, {
     key: "cyclic",
     value: function cyclic(duration) {
-      var _ref4 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-          _ref4$is = _ref4.is,
-          is = _ref4$is === void 0 ? 'step' : _ref4$is,
-          _ref4$min = _ref4.min,
-          min = _ref4$min === void 0 ? 0 : _ref4$min,
-          max = _ref4.max;
+      var _ref5 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+          _ref5$is = _ref5.is,
+          is = _ref5$is === void 0 ? 'step' : _ref5$is,
+          _ref5$min = _ref5.min,
+          min = _ref5$min === void 0 ? 0 : _ref5$min,
+          max = _ref5.max;
 
       this.cast(duration, {
         is: is,
@@ -544,12 +561,12 @@ var Durations = /*#__PURE__*/function () {
   }, {
     key: "interpolate",
     value: function interpolate(ratio) {
-      var _ref5 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-          _ref5$is = _ref5.is,
-          is = _ref5$is === void 0 ? 'step' : _ref5$is,
-          _ref5$min = _ref5.min,
-          min = _ref5$min === void 0 ? 0 : _ref5$min,
-          max = _ref5.max;
+      var _ref6 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+          _ref6$is = _ref6.is,
+          is = _ref6$is === void 0 ? 'step' : _ref6$is,
+          _ref6$min = _ref6.min,
+          min = _ref6$min === void 0 ? 0 : _ref6$min,
+          max = _ref6.max;
 
       var head = this.cast(min || 0, {
         is: is,
@@ -572,11 +589,11 @@ var Durations = /*#__PURE__*/function () {
       var index = this.cyclic(step);
       var state = this.steps[index];
 
-      var _ref6 = state || [],
-          _ref7 = _slicedToArray(_ref6, 3),
-          context = _ref7[0],
-          play = _ref7[1],
-          stop = _ref7[2];
+      var _ref7 = state || [],
+          _ref8 = _slicedToArray(_ref7, 3),
+          context = _ref8[0],
+          play = _ref8[1],
+          stop = _ref8[2];
 
       return {
         beat: context[0],
@@ -594,15 +611,15 @@ var Durations = /*#__PURE__*/function () {
       var _context8,
           _this = this;
 
-      var _ref8 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-          _ref8$is = _ref8.is,
-          is = _ref8$is === void 0 ? 'ms' : _ref8$is,
-          _ref8$units = _ref8.units,
-          units = _ref8$units === void 0 ? ['8n', '4n'] : _ref8$units,
-          _ref8$calc = _ref8.calc,
-          calc = _ref8$calc === void 0 ? 'floor' : _ref8$calc,
-          _ref8$size = _ref8.size,
-          size = _ref8$size === void 0 ? 'min' : _ref8$size;
+      var _ref9 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+          _ref9$is = _ref9.is,
+          is = _ref9$is === void 0 ? 'ms' : _ref9$is,
+          _ref9$units = _ref9.units,
+          units = _ref9$units === void 0 ? ['8n', '4n'] : _ref9$units,
+          _ref9$calc = _ref9.calc,
+          calc = _ref9$calc === void 0 ? 'floor' : _ref9$calc,
+          _ref9$size = _ref9.size,
+          size = _ref9$size === void 0 ? 'min' : _ref9$size;
 
       var durations = _sortInstanceProperty(_context8 = _mapInstanceProperty(units).call(units, function (unit) {
         var value = _this.cast(duration, {
@@ -693,10 +710,10 @@ var Element = /*#__PURE__*/function () {
 
 var Elements = /*#__PURE__*/function () {
   function Elements() {
-    var _ref9 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
-        source = _ref9.source,
-        store = _ref9.store,
-        cast = _ref9.cast;
+    var _ref10 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+        source = _ref10.source,
+        store = _ref10.store,
+        cast = _ref10.cast;
 
     _classCallCheck(this, Elements);
 
@@ -762,20 +779,20 @@ var Elements = /*#__PURE__*/function () {
       throw TypeError('Element id must be a string in the format of "kind.hash"');
     }
   }, {
-    key: "every",
-    value: function every(kind) {
-      var _context13;
-
-      return _mapInstanceProperty(_context13 = _Object$values(this.data[kind])).call(_context13, function (elem) {
-        return new Element(elem);
-      });
-    }
-  }, {
     key: "one",
     value: function one(kind) {
+      var _context13;
+
+      return _everyInstanceProperty(_context13 = this).call(_context13, kind)[0];
+    }
+  }, {
+    key: "every",
+    value: function every(kind) {
       var _context14;
 
-      return _everyInstanceProperty(_context14 = this).call(_context14, kind)[0];
+      return _mapInstanceProperty(_context14 = _Object$values(this.data[kind])).call(_context14, function (elem) {
+        return new Element(elem);
+      });
     }
   }, {
     key: "resolve",
@@ -796,12 +813,12 @@ var Elements = /*#__PURE__*/function () {
 
   }, {
     key: "register",
-    value: function register(_ref10) {
+    value: function register(_ref11) {
       var _context15;
 
-      var kind = _ref10.kind,
-          value = _ref10.value,
-          props = _ref10.props;
+      var kind = _ref11.kind,
+          value = _ref11.value,
+          props = _ref11.props;
       if (!kind || typeof kind !== 'string') throw TypeError('kind must be a non-empty string');
       if (value == null) throw TypeError('value must be defined and non-null');
       var elem = bach.elementize(kind, _concatInstanceProperty(_context15 = [value]).call(_context15, _toConsumableArray(props)));
@@ -825,17 +842,17 @@ var Elements = /*#__PURE__*/function () {
       };
       if (!elements) return null; // TODO: Validate element shape with JSON Schema
 
-      return _reduceInstanceProperty(_context16 = _Object$entries(elements)).call(_context16, function (acc, _ref11) {
+      return _reduceInstanceProperty(_context16 = _Object$entries(elements)).call(_context16, function (acc, _ref12) {
         var _context17;
 
-        var _ref12 = _slicedToArray(_ref11, 2),
-            kind = _ref12[0],
-            ids = _ref12[1];
+        var _ref13 = _slicedToArray(_ref12, 2),
+            kind = _ref13[0],
+            ids = _ref13[1];
 
-        var elems = _reduceInstanceProperty(_context17 = _Object$entries(ids)).call(_context17, function (acc, _ref13) {
-          var _ref14 = _slicedToArray(_ref13, 2),
-              id = _ref14[0],
-              elem = _ref14[1];
+        var elems = _reduceInstanceProperty(_context17 = _Object$entries(ids)).call(_context17, function (acc, _ref14) {
+          var _ref15 = _slicedToArray(_ref14, 2),
+              id = _ref15[0],
+              elem = _ref15[1];
 
           return _objectSpread(_objectSpread({}, acc), {}, _defineProperty({}, id, as(_objectSpread({
             id: id,
@@ -907,8 +924,8 @@ var Beat = /*#__PURE__*/function () {
       var _context20,
           _this5 = this;
 
-      return _flatMapInstanceProperty(_context20 = this.data.items).call(_context20, function (_ref15) {
-        var elements = _ref15.elements;
+      return _flatMapInstanceProperty(_context20 = this.data.items).call(_context20, function (_ref16) {
+        var elements = _ref16.elements;
         return _mapInstanceProperty(elements).call(elements, function (elem) {
           return _this5.store.resolve(elem);
         });
@@ -917,16 +934,16 @@ var Beat = /*#__PURE__*/function () {
   }, {
     key: "kinds",
     get: function get() {
-      return this.all(function (_ref16) {
-        var kind = _ref16.kind;
+      return this.all(function (_ref17) {
+        var kind = _ref17.kind;
         return kind;
       });
     }
   }, {
     key: "values",
     get: function get() {
-      return this.all(function (_ref17) {
-        var value = _ref17.value;
+      return this.all(function (_ref18) {
+        var value = _ref18.value;
         return value;
       });
     }
@@ -1002,8 +1019,8 @@ var Beat = /*#__PURE__*/function () {
   }, {
     key: "notesOf",
     value: function notesOf(elements) {
-      return Note.unite(_flatMapInstanceProperty(elements).call(elements, function (_ref18) {
-        var notes = _ref18.notes;
+      return Note.unite(_flatMapInstanceProperty(elements).call(elements, function (_ref19) {
+        var notes = _ref19.notes;
         return notes;
       }));
     }
@@ -1087,8 +1104,8 @@ var Music = /*#__PURE__*/function () {
       return Note.unite(_flatMapInstanceProperty(_context28 = this.beats).call(_context28, function (beat) {
         var _context29;
 
-        return _flatMapInstanceProperty(_context29 = beat.elements).call(_context29, function (_ref19) {
-          var notes = _ref19.notes;
+        return _flatMapInstanceProperty(_context29 = beat.elements).call(_context29, function (_ref20) {
+          var notes = _ref20.notes;
           return notes;
         });
       }));
@@ -1157,6 +1174,7 @@ exports.Note = Note;
 exports.chordify = chordify;
 exports.clamp = _clamp;
 exports.compose = compose;
+exports.durationsOf = durationsOf;
 exports.gcd = gcd;
 exports.invlerp = invlerp;
 exports.lerp = lerp;
